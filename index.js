efc938216c0040adc114d07e7528bd5e8fd34024
PricneMohamed <mohamedjoba1010@gmail.com>
2024-01-18 02:29:13 -0800

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 let sun = document.querySelector(".sun");
+let h1_name = document.querySelector(".h1_name");
+let conter_h2 = document.querySelector(".conter_h2");
+let btn_about = document.querySelector(".btn_about");
+let ico = document.querySelectorAll(".ico");
 let light = 0;
 sun.addEventListener("click", function () {
   if (light == 0) {
     document.body.style.background = "#fff";
     sun.style.color = "#000";
+    h1_name.style.color = "#000";
+    conter_h2.style.color = "#000";
+    btn_about.style.color = "#000";
+    ico.forEach(function(ele){
+      ele.style.color = "#000";
+    })   
     light = 1;
   } else if (light == 1) {
     document.body.style.background = "#000";
     sun.style.color = "#fff";
+    h1_name.style.color = "#fff";
+    conter_h2.style.color = "#fff";
+    btn_about.style.color = "#fff";
+    ico.forEach(function(ele){
+      ele.style.color = "#fff";
+    })
     light = 0;
   }
 });
